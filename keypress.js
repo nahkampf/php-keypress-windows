6866b4c85232311729bc59496a54f8ab4c7b8975
Sebastian Utbult <kreuzweg@gmail.com>
2024-08-23 03:31:28 +0200

    Update keypress.js
    Remove dependency on keypress lib

--- keypress.js
+++ keypress.js
@@ -1,8 +1,7 @@
-var keypress = require('keypress');
-keypress(process.stdin);
-process.stdin.on('keypress', function (ch, key) {
+let readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.on('keypress', (ch, key) => {
   console.log(JSON.stringify(key));
   process.stdin.pause();
 });
 process.stdin.setRawMode(true);
-process.stdin.resume();
